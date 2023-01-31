9e22c565be0b238cdbaf5964516bbae5f87229f2
truong04052003 <tranvantruong10b2@gmail.com>
2023-01-31 11:47:54 +0700

    migrate-users

--- database/migrations/2023_01_31_044623_create_table_users_table.php
+++ database/migrations/2023_01_31_044623_create_table_users_table.php
@@ -16,10 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('address');
+            $table->string('phone');
+            $table->string('image');
+            $table->string('gender');
+            $table->unsignedBigInteger('group_id');
+            $table->foreign('group_id')->references('id')->on('groups');
             $table->timestamps();
         });
     }
