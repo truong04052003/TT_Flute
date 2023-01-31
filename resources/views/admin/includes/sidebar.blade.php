54a5ad0b2a1c80da9a9645fdc3721d96eacde60e
toandang7112004 <toandang7112004@gmail.com>
2023-01-31 11:23:49 +0700

    layout

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,44 @@
+<aside class="menu-sidebar d-none d-lg-block">
+    <div class="logo">
+        <a href="#">
+            <img src="{{ asset('admin/images/icon/logo.png') }}" alt="" />
+            {{-- Shop TT-Flute --}}
+        </a>
+    </div>
+    <div class="menu-sidebar__content js-scrollbar1">
+        <nav class="navbar-sidebar">
+            <ul class="list-unstyled navbar__list">
+                <li>
+                    <a href="#" class="fas">
+                        Trang Chủ</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Sản Phẩm</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Đơn Hàng</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Khách Hàng</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Loại Sản Phẩm</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Nhân Viên</a>
+                </li>
+                <li>
+                    <a href="#" class="fas">
+                        Quản Lí Quyền</a>
+                </li>
+
+
+            </ul>
+        </nav>
+    </div>
+</aside>
