52ab4873b5f875556606f578bab51ff402c07281
toandang7112004 <toandang7112004@gmail.com>
2023-02-06 14:27:52 +0700

    feature-jwt-api

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Traits\HasPermissions;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable,HasPermissions;
 
@@ -81,5 +81,19 @@ class User extends Authenticatable
             return $query->where('id', 'LIKE', '%' . $request['iduser'] . '%');
         }
         return $query;
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
